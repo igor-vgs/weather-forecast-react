9ef3d21900b5bb5b8be048b768b921c692bb4030
Igor Bakhtin <50614902+igor-vgs@users.noreply.github.com>
2023-09-12 15:19:25 +0300

    Changed bg pics

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ function App() {
 
   return (
     <div className={onTempChange({ weather })}>
-      <main>
-        <Search setWeather={setWeather} />
-        <Weather weather={weather} />
-      </main>
+      <Search setWeather={setWeather} />
+      <Weather weather={weather} />
     </div>
   );
 }
